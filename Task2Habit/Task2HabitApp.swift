4363730828bcc316099003e2e4f2cd256de71dc8
Sean Gilday <spgilday19@gmail.com>
2024-06-29 16:54:59 -0400

    Initial Commit

---
+++ Task2Habit/Task2HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task2HabitApp.swift
+//  Task2Habit
+//
+//  Created by Sean Gilday on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Task2HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
